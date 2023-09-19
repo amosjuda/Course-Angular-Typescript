a84b676d008c56ac93a169cb88b00e12f36ca408
Amos juda <amosjudasilva@gmail.com>
2023-09-19 13:20:57 -0300

    Updating the html page, first and second component created, adding the components to the app modules and importing them

---
+++ src/app/my-first/my-first.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "my-first-component",
+    template: `
+        <p>my first component with Angular 2</p>
+    `
+
+})
+export class MyFirstComponent {} 
