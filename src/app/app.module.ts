d42b4273d941be4b7abc48a81a8573f67c307154
Amos juda <amosjudasilva@gmail.com>
2023-09-20 10:11:20 -0300

    starting modules, showing which ones can be seen and displayed, and creating courses and detail-courses folders.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MyFirstComponent } from './my-first/my-first.component';
 import { MyFirst2Component } from './my-first2/my-first2.component';
+import { CursosModule } from './cursos/cursos.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { MyFirst2Component } from './my-first2/my-first2.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CursosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
